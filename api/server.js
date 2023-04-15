1aa4ae9bb19ebdfe0f6d727b778142435217c4b8
Vllaadd <vlladdiimiir@gmail.com>
2023-04-15 05:06:10 -0400

    models created routes created

--- api/server.js
+++ api/server.js
@@ -6,12 +6,30 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+//connecting to MongoDB•••••••••••••••••••••••••••••••
 mongoose.connect("mongodb+srv://vladzizic:ukvC6AHw4dmyrCml@cluster0.mdqkrcm.mongodb.net/to-do-list", {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
     .then(()=> console.log("Connected to DB"))
     .catch(console.error);
+
+const Todo = require('./models/Todos');
+
+//routes•••••••••••••••••••••••••••••••••••••••••••••
+app.get('/todos', async(req, res) =>{
+    const todos = await Todo.find();
+    res.json(todos);
+});
+
+app.post('/todo/new', (req, res) => {
+        const todo = new Todo({
+            text: req.body.text
+        });
+        todo.save();
+        res.json(todo);
+})
+
 
 app.listen(3001, ()=> console.log("Server started on port 3001"));
 
