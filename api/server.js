7198f3156851bb3cfd91673268acbb786ea41fcb
Vllaadd <vlladdiimiir@gmail.com>
2023-04-15 01:42:17 -0400

    trying to connect to mongoDB

--- api/server.js
+++ api/server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect("mongodb://locahost/to-do-list", {
+mongoose.connect("mongodb://localhost/to-do-list", {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
