5bf2745236f98d5f12a196498f7c32b5b503f5bc
Vllaadd <vlladdiimiir@gmail.com>
2023-04-18 00:38:31 -0400

    first lines in JSX

--- client/src/App.js
+++ client/src/App.js
@@ -4,20 +4,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome, Vlad</h1>
+      <h4>Your Task</h4>
+
+      <div className="todos">
+        <div className="todo">
+          <div className="checkbox"></div>
+
+          <div className="text">Get the bread</div>
+          <div className="delete-todo">x</div>
+        </div>
+      </div>
     </div>
   );
 }
