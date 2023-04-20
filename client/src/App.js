94a93fba3c2ae91b7802405ac7c6934d36308b08
Vllaadd <vlladdiimiir@gmail.com>
2023-04-20 01:29:37 -0400

    changing names which possibly created error when fetching data

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 
-const API_BASE = "http://localhost:3001";
+const API_BASE = "http://localhost:3000";
 
 function App() {
   const [todos, setTodos] = useState([]);
